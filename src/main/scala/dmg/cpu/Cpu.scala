b0b3bba15aa43469b7b175ec9e6e4539a09c9093
diningyo <diningyo.kpuku.jougeki@gmail.com>
2021-01-30 20:48:02 +0900

    added interface definition.

--- src/main/scala/dmg/cpu/Cpu.scala
+++ src/main/scala/dmg/cpu/Cpu.scala
@@ -3,9 +3,13 @@ import chisel3.util._
 
 
 class CpuIO extends Bundle {
-
+  val addr = UInt(16.W)
+  val attr = Bool()
+  val data = UInt(8.W)
 }
 
 class Cpu extends Module {
   val io = IO(new CpuIO())
+
+  io := DontCare
 }
