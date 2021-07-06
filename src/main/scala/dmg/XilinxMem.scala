14715f258b74dde9500c3ca0b086d3fdc2e0e3c4
diningyo <diningyo.kpuku.jougeki@gmail.com>
2021-07-06 23:32:17 +0900

    modified class name(tmp.).

--- src/main/scala/dmg/XilinxMem.scala
+++ src/main/scala/dmg/XilinxMem.scala
@@ -1,7 +1,7 @@
 import chisel3._
 import chisel3.util._
 
-class XilinxMem(val hex_path: String)
+class xilinx_mem(val hex_path: String)
     extends BlackBox with HasBlackBoxResource {
   val io = IO(new Bundle {
     // external
