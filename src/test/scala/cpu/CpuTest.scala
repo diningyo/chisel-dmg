753159540f0904760a6a151bc1c27cb2f99add8c
diningyo <diningyo.kpuku.jougeki@gmail.com>
2021-07-08 23:08:08 +0900

    added test signals to CpuTesttb.

--- src/test/scala/cpu/CpuTest.scala
+++ src/test/scala/cpu/CpuTest.scala
@@ -1,22 +1,28 @@
-import org.scalatest._
-import chiseltest._
 import chisel3._
 
+import org.scalatest._
+import chiseltest._
+import chiseltest.internal.VerilatorBackendAnnotation
+import chiseltest.experimental.TestOptionBuilder._
+
 object BlarggGbTests {
+
+  val testSourceDir = "src/test/resources/blargg-gb-tests"
+
   def get_cpu_instrs_ind_tests(): Seq[String] = {
-    val cpu_instrs_ind_path = "cpu_instrs/individual"
+    val cpu_instrs_ind_path = s"${testSourceDir}/cpu_instrs/individual"
     val cpu_instrs_ind = Seq(
-      "01-special.gb",
-      "02-interrupts.gb",
-      "03-op sp,hl.gb",
-      "04-op r,imm.gb",
-      "05-op rp.gb",
-      "06-ld r,r.gb",
-      "07-jr,jp,call,ret,rst.gb",
-      "08-misc instrs.gb",
-      "09-op r,r.gb",
-      "10-bit ops.gb",
-      "11-op a,(hl).gb",
+      "01-special.hex",
+      //"02-interrupts.gb",
+      //"03-op sp,hl.gb",
+      //"04-op r,imm.gb",
+      //"05-op rp.gb",
+      //"06-ld r,r.gb",
+      //"07-jr,jp,call,ret,rst.gb",
+      //"08-misc instrs.gb",
+      //"09-op r,r.gb",
+      //"10-bit ops.gb",
+      //"11-op a,(hl).gb",
     )
 
     cpu_instrs_ind.map(cpu_instrs_ind_path + "/" + _)
@@ -64,25 +70,40 @@ object BlarggGbTests {
   )
 }
 
-class CpuTestTb(val test_rom: String) extends Module {
+class CpuTestTb(val testRom: String) extends Module {
 
-  val io = IO(new Bundle {})
+  val io = IO(new Bundle {
+    val finish = Output(Bool())
+    val is_success = Output(Bool())
+    val timeout = Output(Bool())
+  })
 
   io := DontCare
 
+  val mem = Module(new Mem(testRom))
+
+  val dut_cpu = Module(new Cpu())
+
+  dut_cpu.io.mem <> mem.io
 }
 
 class CpuTest extends FlatSpec with ChiselScalatestTester with Matchers {
+
+  val annos = Seq(VerilatorBackendAnnotation)
+
   behavior of "Cpu"
 
   import BlarggGbTests._
 
   get_cpu_instrs_ind_tests().foreach { test_name =>
     it should f"be passed ${test_name} tests" in {
-      test(new CpuTestTb(test_name)) { c =>
-        fail()
+      test(new CpuTestTb(test_name)).withAnnotations(annos) { c =>
+        c.clock.setTimeout(10)
+        //println(s"finish = ${c.io.finish.peek()}")
+        while(c.io.finish.peek() != true.B) {
+          c.clock.step(1)
+        }
       }
     }
   }
-
 }
