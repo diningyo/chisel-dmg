cbf9c04f7f00d92e850c46b009b8a09fea8f91be
diningyo <diningyo.kpuku.jougeki@gmail.com>
2021-08-27 22:20:13 +0900

    added all ldr test cases

--- src/test/resources/cpu/000_ld.s
+++ src/test/resources/cpu/000_ld.s
@@ -7,11 +7,73 @@
 	.romBankSize   $4000 ; generates $8000 byte ROM
 	.romBanks      2
 
-	.org $100
-	  ld a, $a5                  ; a = $a5
+	.org $150
+    ;; ld r, a
+	  ld a, $01                  ; a = $01
+	  ld a, a
 	  ld b, a
+	  ld c, a
+	  ld d, a
+	  ld e, a
+	  ld h, a
+	  ld l, a
+
+    ;; ld r, b
+	  ld b, $02                  ; b = $02
+	  ld a, b
+	  ld b, b
 	  ld c, b
+	  ld d, b
+	  ld e, b
+	  ld h, b
+	  ld l, b
+
+    ;; ld r, c
+    ld c, $03                  ; c = $03
+	  ld a, c
+	  ld b, c
+	  ld c, c
 	  ld d, c
+	  ld e, c
+	  ld h, c
+	  ld l, c
+
+    ;; ld r, d
+    ld d, $04                  ; d = $04
+	  ld a, d
+	  ld b, d
+	  ld c, d
+	  ld d, d
 	  ld e, d
+	  ld h, d
+	  ld l, d
+
+    ;; ld r, e
+    ld e, $05                  ; e = $05
+	  ld a, e
+	  ld b, e
+	  ld c, e
+	  ld d, e
+	  ld e, e
 	  ld h, e
+	  ld l, e
+
+    ;; ld r, h
+    ld h, $06                  ; h = $06
+	  ld a, h
+	  ld b, h
+	  ld c, h
+	  ld d, h
+	  ld e, h
+	  ld h, h
 	  ld l, h
+
+    ;; ld r, l
+    ld l, $07                  ; l = $07
+	  ld a, l
+	  ld b, l
+	  ld c, l
+	  ld d, l
+	  ld e, l
+	  ld h, l
+	  ld l, l
